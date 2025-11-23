0b61bc2fcce9cdfaf3271f150cad6341b80800cc
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-23 18:45:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,33 +3,33 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+variable: "--font-geist-sans",
+subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+variable: "--font-geist-mono",
+subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "LokNetaa Poster Editor Software",
-  description: "Develop By ARP Digital Solution.",
+title: "LokNetaa Poster Editor Software",
+description: "Design packy Media Services.",
 };
 
 export default function RootLayout({
-  children,
+children,
 }: Readonly<{
-  children: React.ReactNode;
+children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  );
+return (
+<html lang="en">
+<body
+className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+>
+{children}
+<Footer/>
+</body>
+</html>
+);
 }
